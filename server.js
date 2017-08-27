a9c9584259661526d2ad6bd54d86d7649482f624
James Hurt <hurtja15@gmail.com>
2017-08-27 14:28:40 -0500

    comments

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+/*************************************
+ * DEPENDENCIES
+ **********************************/
 var express = require("express")
 var bodyParser = require("body-parser")
 
+/************************************
+ * ExpressJS Setup
+ ************************************/
 app = express()
-
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(bodyParser.json())
-
-
 app.listen("8000");
+
+/***********************************
+ * ROUTING
+ **********************************/
 
 app.get("/", function (req, res) {
     res.send("Hello World");
