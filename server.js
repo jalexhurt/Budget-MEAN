a54c1e410d0120826f7254334585d42365820de6
James Hurt <hurtja15@gmail.com>
2017-08-27 14:46:50 -0500

    get_response

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
  * DEPENDENCIES
  **********************************/
 var express = require("express")
+var fs = require("fs")
 var bodyParser = require("body-parser")
+var pug = require("pug")
 
 /************************************
  * ExpressJS Setup
@@ -12,10 +14,23 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 app.listen("8000");
 
+/*********************************
+ * GLOBAL Functions
+ *******************************/
+ function get_response(body, title) {
+     if(!title || typeof title === 'undefined') {
+         title = "Budget Application";
+     }
+     var response = fs.readFileSync("html/header.html") + pug.parseFile("pug/title.pug", {title: title});
+     response += body;
+     response += fs.readFileSync("html/footer.html");
+     return response;
+ }
+
 /***********************************
  * ROUTING
  **********************************/
 
 app.get("/", function (req, res) {
-    res.send("Hello World");
+    res.send(get_response("Hello World"));
 })
