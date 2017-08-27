968e1f900f5c8a586e541bead91fd07817540668
James Hurt <hurtja15@gmail.com>
2017-08-27 14:14:57 -0500

    server with express

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+var express = require("express")
+var bodyParser = require("body-parser")
+
+app = express()
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json())
+
+
+app.listen("8000");
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+})
