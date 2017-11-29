06563b17d238158e8223ec8783fda802b658b68e
James Hurt <jah3xc@mail.missouri.edu>
2017-11-29 15:34:34 -0600

    Make visualization better

--- js/app.js
+++ js/app.js
@@ -1,259 +1,274 @@
 var app = angular.module("Budget", []);
 
-var basic_dialog = 
-{
-        modal: true,
-        resizable: false,
-        draggable: false,
+var basic_dialog = {
+  modal: true,
+  resizable: false,
+  draggable: false
 };
 
 app.controller("HomePageController", [
-    "$http", "$location", "$window",
-    function ($http, $location, $window) {
-        //need to load from db
-        var s = this;
+  "$http",
+  "$location",
+  "$window",
+  function($http, $location, $window) {
+    //need to load from db
+    var s = this;
 
-        this.reload_data = function () {
-            $http.post("/get-transactions").then(function (res) {
-                s.transactions = res.data;
-                s.first_id = res.data[0].id;
-                for (var i = 0; i < s.transactions.length; i++) {
-                    s.transactions[i].date = s.transactions[i].date.substring(0, 10);
-                    delete s.transactions[i].id;
-                }
-
-                s.load_stats();
-
-            });
-        };
-
-        this.load_stats = function () {
-            var total = 0;
-            var h = -1;
-            var unsignedTotal = 0;
-            for (var i = 0; i < s.transactions.length; i++) {
-                var a = parseFloat(s.transactions[i].amount);
-                unsignedTotal += Math.abs(a);
-                total += s.transactions[i].type.toUpperCase() == "CREDIT" ? a : -1 * a;
-                h = a > h ? a : h;
-            }
-            s.balance = total;
-            s.high = h;
-            s.average = unsignedTotal / s.transactions.length;
-
-            if (s.transactions.length == 0) {
-                s.balance = s.high = s.average = 0;
-            }
-        };
-        this.reload_data();
-        this.toValidDate = function (d) {
-            return d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
-        };
-        this.addTransaction = function () {
-
-            var data = {
-                data: [s.toValidDate(s.date), s.description, s.amount, s.type]
-            };
-            $http.post("/add", data).then(
-                function (res) {
-                    $("#add-transaction").dialog("close");
-                    $("<div>Success!</div>").dialog({
-                        modal: true,
-                        resizable: false,
-                        draggable: false,
-                        title: "The transaction was successfully inserted"
-                    });
-                    s.reload_data();
-                },
-                function (err) {
-                    $("#add-transaction").dialog("close");
-                    $("<div>Failure!</div>").dialog({
-                        modal: true,
-                        resizable: false,
-                        draggable: false,
-                        title: "The transaction failed to be added"
-                    });
-                }
-            );
-        };
-
-        this.deleteTransaction = function () {
-            var i = parseInt(this.transDelete);
-            var removed = this.transactions.splice(i, 1);
-            this.load_stats();
-            $http.post("/delete-transaction", removed).then(function (res) {
-                $("#delete-transaction").dialog("close");
-                $("<div>Success!</div>").dialog({
-                    modal: true,
-                    resizable: false,
-                    draggable: false,
-                    title: "The transaction was successfully deleted"
-                });
-                s.reload_data();
-            }, function (err) {
-                $("#delete-transaction").dialog("close");
-                $("<div>Failure!</div>").dialog({
-                    modal: true,
-                    resizable: false,
-                    draggable: false,
-                    title: "The transaction failed to be added"
-                });
-            })
-        };
-
-        this.showAddTransaction = function () {
-            $("#add-transaction").dialog({
-                modal: true,
-                resizable: false,
-                draggable: false,
-                title: "Add Transaction"
-            });
-        };
-
-        this.showDeleteTransaction = function () {
-            $("#delete-transaction").dialog({
-                modal: true,
-                resizable: false,
-                draggable: false,
-                title: "Delete Transaction"
-            });
-        };
-
-        this.editTransaction = function (i) {
-            // console.log(i)
-            $window.location = "/edit-transaction?id=" + (this.first_id + i - 1);
-        };
-
-        this.show_graph = function () {
-            var a = [];
-
-            this.transactions.forEach(function (t) {
-                a.push(t.amount);
-            });
-            create_graph(a, "test-id");
+    this.reload_data = function() {
+      $http.post("/get-transactions").then(function(res) {
+        s.transactions = res.data;
+        s.first_id = res.data[0].id;
+        for (var i = 0; i < s.transactions.length; i++) {
+          s.transactions[i].date = s.transactions[i].date.substring(0, 10);
+          delete s.transactions[i].id;
         }
 
-        this.submitVisualize = function () {
-            this.reload_data();
-            var today = new Date();
-            var days = 0;
-            var num = parseInt(this.num);
-            if (this.length == "months") {
-                days = num * 30;
-            }
-            else if (this.length == "weeks") {
-                days = num * 7;
-            }
-            else {
-                days = num;
-            }
+        s.load_stats();
+      });
+    };
 
-            var newDay = new Date();
-            while (Math.ceil(Math.abs(today.getTime() - newDay.getTime()) / (1000 * 3600 * 24)) < days) {
-                newDay.setDate(newDay.getDate() - 1);
-            }
+    this.load_stats = function() {
+      var total = 0;
+      var h = -1;
+      var unsignedTotal = 0;
+      for (var i = 0; i < s.transactions.length; i++) {
+        var a = parseFloat(s.transactions[i].amount);
+        unsignedTotal += Math.abs(a);
+        total += s.transactions[i].type.toUpperCase() == "CREDIT" ? a : -1 * a;
+        h = a > h ? a : h;
+      }
+      s.balance = total;
+      s.high = h;
+      s.average = unsignedTotal / s.transactions.length;
 
-            var data = [];
-            for (var i = 0; i < this.transactions.length; i++) {
-                var temp_date = this.transactions[i].date + "";
-                var year = parseInt(temp_date.substring(0, 4));
-                var month = parseInt(temp_date.substring(5, 7));
-                var day = parseInt(temp_date.substring(8));
-                var new_date = year + "-" + month + "-" + day;
-                var d = Date.parse(new_date);
-                if (d >= newDay && d <= today) {
-                    data.push(parseFloat(this.transactions[i].amount));
-                }
-            }
-
-            if (data.length > 0) {
-                create_graph(data, 'chart');
-            } else {
-                $("<div>No transactions occurred in the time specified</div>").dialog({
-                    modal: true,
-                    draggable: false,
-                    title: "Not Found",
-                    resizable: false
-                })
-            }
-        };
-
-
-        this.submitVisualizeTwoDates = function () {
-            var date1 = this.date1;
-            var date2 = this.date2;
-            this.reload_data();
-            var data = [];
-            for (var i = 0; i < this.transactions.length; i++) {
-                var temp_date = this.transactions[i].date + "";
-                var year = parseInt(temp_date.substring(0, 4));
-                var month = parseInt(temp_date.substring(5, 7));
-                var day = parseInt(temp_date.substring(8));
-                var new_date = year + "-" + month + "-" + day;
-                var d = Date.parse(new_date);
-                if (d >= date1 && d <= date2) {
-                    data.push(parseFloat(this.transactions[i].amount));
-                }
-            }
-
-            if (data.length > 0) {
-                create_graph(data, 'chart');
-            } else {
-                $("<div>No transactions occurred in the time specified</div>").dialog({
-                    modal: true,
-                    draggable: false,
-                    title: "Not Found",
-                    resizable: false
-                })
-            }
+      if (s.transactions.length == 0) {
+        s.balance = s.high = s.average = 0;
+      }
+    };
+    this.reload_data();
+    this.toValidDate = function(d) {
+      return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+    };
+    this.addTransaction = function() {
+      var data = {
+        data: [s.toValidDate(s.date), s.description, s.amount, s.type]
+      };
+      $http.post("/add", data).then(
+        function(res) {
+          $("#add-transaction").dialog("close");
+          $("<div>Success!</div>").dialog({
+            modal: true,
+            resizable: false,
+            draggable: false,
+            title: "The transaction was successfully inserted"
+          });
+          s.reload_data();
+        },
+        function(err) {
+          $("#add-transaction").dialog("close");
+          $("<div>Failure!</div>").dialog({
+            modal: true,
+            resizable: false,
+            draggable: false,
+            title: "The transaction failed to be added"
+          });
         }
-    }
+      );
+    };
 
+    this.deleteTransaction = function() {
+      var i = parseInt(this.transDelete);
+      var removed = this.transactions.splice(i, 1);
+      this.load_stats();
+      $http.post("/delete-transaction", removed).then(
+        function(res) {
+          $("#delete-transaction").dialog("close");
+          $("<div>Success!</div>").dialog({
+            modal: true,
+            resizable: false,
+            draggable: false,
+            title: "The transaction was successfully deleted"
+          });
+          s.reload_data();
+        },
+        function(err) {
+          $("#delete-transaction").dialog("close");
+          $("<div>Failure!</div>").dialog({
+            modal: true,
+            resizable: false,
+            draggable: false,
+            title: "The transaction failed to be added"
+          });
+        }
+      );
+    };
+
+    this.showAddTransaction = function() {
+      $("#add-transaction").dialog({
+        modal: true,
+        resizable: false,
+        draggable: false,
+        title: "Add Transaction"
+      });
+    };
+
+    this.showDeleteTransaction = function() {
+      $("#delete-transaction").dialog({
+        modal: true,
+        resizable: false,
+        draggable: false,
+        title: "Delete Transaction"
+      });
+    };
+
+    this.editTransaction = function(i) {
+      // console.log(i)
+      $window.location = "/edit-transaction?id=" + (this.first_id + i - 1);
+    };
+
+    this.show_graph = function() {
+      var a = [];
+
+      this.transactions.forEach(function(t) {
+        a.push(t.amount);
+      });
+      create_graph(a, "test-id");
+    };
+
+    this.submitVisualize = function() {
+      this.reload_data();
+      var today = new Date();
+      var days = 0;
+      var num = parseInt(this.num);
+      if (this.length == "months") {
+        days = num * 30;
+      } else if (this.length == "weeks") {
+        days = num * 7;
+      } else {
+        days = num;
+      }
+
+      var newDay = new Date();
+      while (
+        Math.ceil(
+          Math.abs(today.getTime() - newDay.getTime()) / (1000 * 3600 * 24)
+        ) < days
+      ) {
+        newDay.setDate(newDay.getDate() - 1);
+      }
+
+      var data = [];
+      for (var i = 0; i < this.transactions.length; i++) {
+        var temp_date = this.transactions[i].date + "";
+        var year = parseInt(temp_date.substring(0, 4));
+        var month = parseInt(temp_date.substring(5, 7));
+        var day = parseInt(temp_date.substring(8));
+        var new_date = year + "-" + month + "-" + day;
+        var d = Date.parse(new_date);
+        if (d >= newDay && d <= today) {
+          data.push(parseFloat(this.transactions[i].amount));
+        }
+      }
+
+      if (data.length > 0) {
+        var settings = basic_dialog;
+        settings.width = "90%";
+        settings.title = "Transaction Statistics";
+        $("#chart-popup").dialog(basic_dialog);
+        create_graph(data, "chart");
+      } else {
+        $("<div>No transactions occurred in the time specified</div>").dialog({
+          modal: true,
+          draggable: false,
+          title: "Not Found",
+          resizable: false
+        });
+      }
+    };
+
+    this.submitVisualizeTwoDates = function() {
+      var date1 = this.date1;
+      var date2 = this.date2;
+      this.reload_data();
+      var data = [];
+      for (var i = 0; i < this.transactions.length; i++) {
+        var temp_date = this.transactions[i].date + "";
+        var year = parseInt(temp_date.substring(0, 4));
+        var month = parseInt(temp_date.substring(5, 7));
+        var day = parseInt(temp_date.substring(8));
+        var new_date = year + "-" + month + "-" + day;
+        var d = Date.parse(new_date);
+        if (d >= date1 && d <= date2) {
+          data.push(parseFloat(this.transactions[i].amount));
+        }
+      }
+
+      if (data.length > 0) {
+        create_graph(data, "chart");
+      } else {
+        $("<div>No transactions occurred in the time specified</div>").dialog({
+          modal: true,
+          draggable: false,
+          title: "Not Found",
+          resizable: false
+        });
+      }
+    };
+  }
 ]);
 
-app.directive("addTransaction", function () {
-    return {
-        templateUrl: "html/add-transaction-form.html"
-    };
+app.directive("addTransaction", function() {
+  return {
+    templateUrl: "html/add-transaction-form.html"
+  };
 });
 
-app.directive("transactionTable", function () {
-    return {
-        templateUrl: "html/transaction_table.html"
-    };
+app.directive("transactionTable", function() {
+  return {
+    templateUrl: "html/transaction_table.html"
+  };
 });
 
-app.directive("deleteTransaction", function () {
-    return {
-        templateUrl: "html/delete-transaction-form.html"
-    };
+app.directive("deleteTransaction", function() {
+  return {
+    templateUrl: "html/delete-transaction-form.html"
+  };
 });
 
-app.controller("createAccountController", ['$http', function ($http) {
-    this.submitForm = function () {
-        var username = this.username;
-        var password = this.password;
-        var confirm = this.confirm;
+app.controller("createAccountController", [
+  "$http",
+  function($http) {
+    this.submitForm = function() {
+      var username = this.username;
+      var password = this.password;
+      var confirm = this.confirm;
 
-        $("#create-account").dialog('close');
-                
-        if(!username || !password || !confirm || password != confirm) {
-            $("<div>Invalid Form!</div>").dialog(basic_dialog)            
-        }
-        else {
-            $http.post("/createaccount", {username: username, password: password}).then(function(res) {
-                $("<div>Success!<br>The account was created</div>").dialog(basic_dialog);
-            }, function(err) {
-                $("<div>Error! An error occurred</div>").dialog(basic_dialog);
-            })
-        }
-    }
-}])
+      $("#create-account").dialog("close");
+
+      if (!username || !password || !confirm || password != confirm) {
+        $("<div>Invalid Form!</div>").dialog(basic_dialog);
+      } else {
+        $http
+          .post("/createaccount", { username: username, password: password })
+          .then(
+            function(res) {
+              $("<div>Success!<br>The account was created</div>").dialog(
+                basic_dialog
+              );
+            },
+            function(err) {
+              $("<div>Error! An error occurred</div>").dialog(basic_dialog);
+            }
+          );
+      }
+    };
+  }
+]);
 
 /**********************************************************************************
  * GENERAL PURPOSE
  */
 
 function showDialog(id) {
-    $('#' + id).dialog(basic_dialog)
+  $("#" + id).dialog(basic_dialog);
 }
--- js/charts.js
+++ js/charts.js
@@ -1,51 +1,53 @@
 // Create data -array of values to visualize
 function create_graph(data, id) {
-    var dataset = data
-    $("#" + id).html("");
-    var w = 500;
-    var h = 100;
-    var barPadding = 1;
+  var dataset = data;
+  $("#" + id).html("");
+  var w = 700;
+  var h = 300;
+  var barPadding = 1;
 
-    //Create SVG element
-    var svg = d3.select("#" + id)
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+  //Create SVG element
+  var svg = d3
+    .select("#" + id)
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-    svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("x", function(d, i) {
-            return i * (w / dataset.length);
-        })
-        .attr("y", function(d) {
-            return h - (d * 4);
-        })
-        .attr("width", w / dataset.length - barPadding)
-        .attr("height", function(d) {
-            return d * 4;
-        })
-        .attr("fill", function(d) {
-            return "rgb(0, 0, " + (d * 10) + ")";
-        });
+  svg
+    .selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+      return i * (w / dataset.length);
+    })
+    .attr("y", function(d) {
+      return h - d * 4;
+    })
+    .attr("width", w / dataset.length - barPadding)
+    .attr("height", function(d) {
+      return d * 4;
+    })
+    .attr("fill", function(d) {
+      return "rgb(0, 0, " + d * 10 + ")";
+    });
 
-    svg.selectAll("text")
-        .data(dataset)
-        .enter()
-        .append("text")
-        .text(function(d) {
-            return d;
-        })
-        .attr("text-anchor", "middle")
-        .attr("x", function(d, i) {
-            return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-        })
-        .attr("y", function(d) {
-            return h - (d * 4) + 14;
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("fill", "white");
-
+  svg
+    .selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function(d, i) {
+      return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+    })
+    .attr("y", function(d) {
+      return h - d * 4 + 14;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "white");
 }
